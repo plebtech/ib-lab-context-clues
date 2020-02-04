528105190933b19d983adc780bc99f6ba0b44abc
Nick Mc <nbmc@protonmail.com>
2020-02-04 13:11:54 -0600

    cleaned up working attempt.

--- scripts.js
+++ scripts.js
@@ -15,37 +15,4 @@ function accuse(index) {
     });
 }
 
-// old.
-
-// for (var i = 100; i > 0; i--) {
-//     var index = logIndex(i); // capture i with closure.
-//     // console.log(index);
-//     var accusation = mkAccuse(index); // this function does the heavy lifting.
-//     accusation; // instantiate the accusation.
-// }
-
-// // capture i through closure.
-// function logIndex(index) {
-//     return index;
-// }
-
-// function mkAccuse(index) {
-//     var friends = ['Odo', 'Rufus', 'Horace', 'Justin', 'Isabel'];
-//     var locations = ['cloak room', 'antechamber', 'mud room', 'root cellar', 'library', 'study', 'formal dining room', 'den', 'shower stall', 'boudoir'];
-//     var weapons = ['hammer', 'revolver', 'antique sword', 'letter opener', 'fire extinguisher', 'bag of potatoes', 'brick', 'large trout', 'fish bone', 'banana', 'lamp', 'paperweight', 'bookend', 'fishing rod', 'arsenic', 'noxious odor', 'pestilence', 'halitosis', 'shard of broken mirror', 'ugly look'];
-//     var cFriend = friends[index % friends.length];
-//     var cLocation = locations[index % locations.length];
-//     var cWeapon = weapons[index % weapons.length];
-//     var h3 = mkH3(index, cFriend, cLocation, cWeapon);
-//     h3;
-// }
-
-// function mkH3(i, f, l, w) {
-//     var fullTag = `<h3 id="${i}">Accusation ${i}</h3>`;
-//     $('body').prepend(fullTag);
-//     $(`#${i}`).click(function() {
-//         alert(`I accuse: ${f} in the ${l} with the ${w}!`);
-//     })
-// }
-
 console.log("Loaded.");
