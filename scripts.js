3d9276b8de8283b45fbe072110394857aa8b9823
Nick Mc <nbmc@protonmail.com>
2020-02-04 13:53:59 -0600

    obfuscated all variables within overarching closure.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
 console.log("Loading..");
 
-for (var i = 100; i > 0; i--) {
-    var btn = accuse(i);
-    btn;
+// at its most basic, a closure is a function that returns another function.
+
+var runIt = closure();
+runIt();
+// overarching closure to obfuscate all variables.
+function closure() {
+    function toReturn() {
+        for (var i = 100; i > 0; i--) {
+            var accusation = accuse(i);
+            accusation;
+        }
+    }
+    return toReturn;
 }
 
 function accuse(index) {
     var friends = ['Odo', 'Rufus', 'Horace', 'Justin', 'Isabel'];
     var locations = ['cloak room', 'antechamber', 'mud room', 'root cellar', 'library', 'study', 'formal dining room', 'den', 'shower stall', 'boudoir'];
     var weapons = ['hammer', 'revolver', 'antique sword', 'letter opener', 'fire extinguisher', 'bag of potatoes', 'brick', 'large trout', 'fish bone', 'banana', 'lamp', 'paperweight', 'bookend', 'fishing rod', 'arsenic', 'noxious odor', 'pestilence', 'halitosis', 'shard of broken mirror', 'ugly look'];
-    $('#container').prepend(`<h3 id="${i}">Accusation ${i}</h3>`);
-    $(`#${i}`).click(function() {
+    $('#container').prepend(`<h3 id="${index}">Accusation ${index}</h3>`);
+    $(`#${index}`).click(function () {
         alert("I accuse: " + friends[index % friends.length] + " in the " + locations[index % locations.length] + " with the " + weapons[index % weapons.length] + "!");
     });
 }
